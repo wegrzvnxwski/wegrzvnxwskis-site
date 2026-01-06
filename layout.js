7078bd69109b1265694b8ecf9d8efb33ae254aac
wegrzvnxwski <wegrzvnxwski@proton.me>
2026-01-06 20:54:37 +0100

    Lekkie poprawki i przygotowania pod dalszą rozbudowę

--- layout.js
+++ layout.js
@@ -17,7 +17,8 @@
                     <li><a href="aboutme.html" title="O mnie" class="nav-link"><img src="images/aboutme.png" alt="O mnie"></a></li>
                     <li><a href="galeria.html" title="Galeria" class="nav-link"><img src="images/galeria.png" alt="Galeria"></a></li>
                     <li><a href="albums.html" title="Albumy, które siadły" class="nav-link"><img src="images/albums.png" alt="Albumy, które siadły"></a></li>
-                    <!-- <li><a href="portfolio.html" title="Portfolio i doświadczenie zawodowe (aka chwaliposty)" class="nav-link"><img src="" alt=""></a></li> TBA -->
+                    <!-- <li><a href="portfolio.html" title="Portfolio i doświadczenie zawodowe (aka chwaliposty)" class="nav-link"><img src="" alt=""></a></li> TBA 
+                    <li><a href="en/" title="English version" class="nav-link"><img src="" alt="">-->
                 </ul>
     `;
     const lo = document.getElementById("navbarek");
